7f6e84a7a8037ab167b9567301e335a2dd6ab122
Jessica <jesli@g.hmc.edu>
2026-09-06 17:59:01 -0700

    edit max count to 10 million
    corrected some syntax errors, enable and reset signals for counter

--- fpga/iCE40_lab1/source/impl_1/counter_2_4Hz.sv
+++ fpga/iCE40_lab1/source/impl_1/counter_2_4Hz.sv
@@ -3,25 +3,30 @@
 // This is a module used in the top-leve module "lab1_jl"
 // It contains the sequential logic for an output of led[2] blinking at 2.4 Hz
 
-module counter_2_4Hz(
-	input   logic   clk, reset, en,
+module counter_2_4Hz
+	#(parameter WIDTH = 25,
+		EN = 1) (
+	input   logic   clk, rst,
 	output  logic   led
 );
 	// logic pulse;
 	logic led_state = 0;
-	logic [24:0] counter = 0;
-	const logic [24:0] max_count = 25'b00_1001_1000_1001_0110_1000_000; // max counter 5 million to convert 5 million cycles into one cycle (at a pace of 2.4 Hz)
+	logic [WIDTH:0] counter = 0;
+	const logic [WIDTH:0] max_count = 10_000_000; // max counter 10 million to convert 10 million cycles into one cycle (at a pace of 2.4 Hz)
 	
 	always_ff @(posedge clk) begin
-			if (reset == 0) counter <= 0;
-			else if (en) begin
+			if (rst == 0) begin
+				counter <= 0;
+				led_state <= 0;
+			end
+			else if (EN) begin
 				if (counter >= max_count) begin
 					led_state <= ~led_state; // toggles led state on or off once cycle time up
 					counter <= 0;
-					end
-				else counter <= counter + 1;
+				end
+				else counter <= counter + 1'b1;
 			end 
-		end
+	end
 		
 	assign led = led_state;
 
--- fpga/iCE40_lab1/source/impl_1/lab1_jl.sv
+++ fpga/iCE40_lab1/source/impl_1/lab1_jl.sv
@@ -6,25 +6,32 @@
 
 
 module lab1_jl(
-	input   logic   reset,
-	input   logic   enable,
 	input   logic   [3:0] s,
-	output  logic led[2:0],
+	output  logic [2:0] led,
 	output  logic [6:0] seg
 );
 
 	logic int_osc;
+	assign reset = 1'b1; // make into button input later?
 	
-	// Internal high-speed oscillator
+	// Internal high-speed oscillator, 48 MHz clock generated in FPGA by HSOSC primitive
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
 	assign led[0] = s[0] ^ s[1];
 	assign led[1] = s[2] & s[3];
 	
 	// switch-to-7 segment display module
-	switch_to_display seven_seg(s, seg);
+	switch_to_display seven_seg(
+		.switch (s), 
+		.segment (seg)
+	);
+	
 	// counter for sequential LED 2.4 Hz blinker
-	counter_2_4Hz blinker(reset, enable, int_osc, led[2]);
+	counter_2_4Hz blinker(
+		.clk (int_osc), 
+		.rst (reset),  
+		.led (led[2])
+	);
 	
 
 endmodule
